f7d37c80adf8ae8f2beaaa479e587969cda79214
Bap2907 <thong2003br@gmail.com>
2025-05-22 14:44:01 +0700

    update layout item purchase

--- app/src/main/java/com/example/futasbus/Adapter/PurchaseAdapter.java
+++ app/src/main/java/com/example/futasbus/Adapter/PurchaseAdapter.java
@@ -1,11 +1,14 @@
 package com.example.futasbus.Adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import com.example.futasbus.R;
 import com.example.futasbus.helper.DateTimeHelper;
@@ -36,7 +39,7 @@ public class PurchaseAdapter extends ArrayAdapter<PurchaseResponse> {
         TextView tvThoiDiemDi = convertView.findViewById(R.id.tvThoiDiemDi);
         TextView tvSoLuongVe = convertView.findViewById(R.id.tvSoLuongVe);
         TextView tvTongTien = convertView.findViewById(R.id.tvTongTien);
-
+        TextView tvTrangThai = convertView.findViewById(R.id.tvTrangThai);
         if (purchase != null) {
             tvTenTuyen.setText(purchase.getTenTuyen());
 
@@ -48,6 +51,30 @@ public class PurchaseAdapter extends ArrayAdapter<PurchaseResponse> {
             NumberFormat format = NumberFormat.getInstance(new Locale("vi", "VN"));
             String formattedPrice = format.format(purchase.getTongTien()) + "đ";
             tvTongTien.setText("Tổng tiền: " + formattedPrice);
+            int trangThai = purchase.getTrangThai();
+
+            switch (trangThai) {
+                case 0:
+                    tvTrangThai.setText("Đã Hủy");
+                    tvTrangThai.setBackgroundColor(ContextCompat.getColor(context, R.color.futa_red));
+                    break;
+                case 1:
+                    tvTrangThai.setText("Đã Đặt");
+                    tvTrangThai.setBackgroundColor(ContextCompat.getColor(context, R.color.light_gray));
+                    break;
+                case 2:
+                    tvTrangThai.setText("Chờ thanh toán");
+                    tvTrangThai.setBackgroundColor(ContextCompat.getColor(context, R.color.yelow));
+                    break;
+                case 3:
+                    tvTrangThai.setText("Đã thanh toán");
+                    tvTrangThai.setBackgroundColor(ContextCompat.getColor(context, R.color.green_futa));
+                    break;
+                case 4:
+                    tvTrangThai.setText("Đã hoàn thành");
+                    tvTrangThai.setBackgroundColor(ContextCompat.getColor(context, R.color.accent));
+                    break;
+            }
         }
         return convertView;
     }
